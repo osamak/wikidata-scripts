0a5c6cf8ed34730fd3bd34bc20a04e0edd7f3664
Osama Khalid <osamak@gnu.org>
2020-07-05 23:47:33 +0300

    Add license notice

--- add_youtube_start_date.py
+++ add_youtube_start_date.py
@@ -1,3 +1,9 @@
+# A script to fetch YouTube channel start dates and generate QuickStatements
+# Copyright (C) 2020  Osama Khalid
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 from datetime import datetime
 from http.client import HTTPException
 from lxml import objectify
